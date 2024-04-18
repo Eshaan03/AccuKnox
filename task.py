de47153f7a673caa3b972ce75d02f333f9605d49
Eshaan03 <eshaanbhargava2001@gmail.com>
2024-04-18 20:09:40 +0530

    class created.

---
+++ task.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+rect_1 = Rectangle(10,20)
+rect_2 = Rectangle(20,30)
+
+rectangles = [rect_1, rect_2]
+
+for rect in rectangles:
+    print(f"The length of the rectangle is {rect.length} and the breadth is {rect.breadth}")
+

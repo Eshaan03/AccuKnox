90baf64ca7b79f2208485fb5fa6a380949548f1d
Eshaan03 <eshaanbhargava2001@gmail.com>
2024-04-18 20:26:07 +0530

    Handled value type in the class

--- task.py
+++ task.py
@@ -1,9 +1,13 @@
 class Rectangle:
     def __init__(self, length, breadth):
-        self.length = length
-        self.breadth = breadth
 
-rect_1 = Rectangle(10,20)
+        if not (isinstance(length, int) or isinstance(breadth, int)):
+            raise TypeError("The values should be integer!")
+        else:
+            self.length = length
+            self.breadth = breadth
+
+rect_1 = Rectangle("10","20")
 rect_2 = Rectangle(20,30)
 
 rectangles = [rect_1, rect_2]
